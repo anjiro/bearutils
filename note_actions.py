846b3eb458ce367a0750a4f6b44b5920ab1286aa
Daniel Ashbrook <dan@danielashbrook.com>
2021-02-12 16:18:19 +0100

    bug fixes, add collect action

--- note_actions.py
+++ note_actions.py
@@ -1,6 +1,7 @@
 from bear import NotesProcessor
 from utils import *
 from collections import defaultdict
+import inspect
 
 
 class NoteActions(NotesProcessor):
@@ -10,6 +11,7 @@ class NoteActions(NotesProcessor):
 			'always_section': '## Always',
 			'search_section': '## Searches',
 			'clear_section': '### Uncheck to clear highlights',
+			'collect_section': '## Collected notes',
 		}
 		self.options.update(options)
 		self.note_actions = defaultdict(list)
@@ -17,6 +19,8 @@ class NoteActions(NotesProcessor):
 		#Compiled regex will be used with .search()
 		self.actions = {
 			re.compile('^\*\s+(?P<do>append|prepend)\s+to\s+(?P<to>titles|notes)\s+in\s+`?' + tag_re + '`?\s*:(?P<what>.+)', flags=re.I): self.append_prepend,
+			
+			re.compile('^\*\s+(?P<do>remove)\s+from\s+(?P<to>titles|notes)\s+in\s+`?' + tag_re + '`?\s*:(?P<what>.+)', flags=re.I): self.append_prepend,
 			
 			re.compile('^\*\s+collect\s+`?' + tag_re + '`?\s+in\s+\[\[(?P<dest>.+?)]]', flags=re.I): self.collect,
 			
@@ -40,29 +44,43 @@ class NoteActions(NotesProcessor):
 					print(match, match.groupdict())
 					func(match, notes)
 				
-		return {note: True for note in self.note_actions}
+		# Don't make changes to the action file
+		return {note: True for note in self.note_actions if note.title != self.options['always_section']}
 		
 		
 	def render(self, note):
 		c = note.contents
 		for func in self.note_actions[note]:
-			print('action:')
-			print(c)
+			print(f'{note.title}: {inspect.getsource(func)}')
 			c = func(c)
 		return c
 		
 		
 	def collect(self, match, notes):
 		#Here we might make a new note with no id and add it to note actions
-		pass
+		tag, dest = match.group(*'tag dest'.split())
+		existing_links = notes[dest].links if dest in notes else []
+		new_links = []
+		for note in notes.values():
+			if tag.strip('#') not in note.tags or note.title == dest or note.title in existing_links:
+				continue
+			new_links.append(note.title)
+			
+		links_body = '\n'.join([self.options['collect_section']] + [f'* [[{link}]]' for link in existing_links])
 		
+		if dest in notes:
+			note = notes[dest]
+			self.note_actions[note].append(lambda c:
+				replace_section(c, self.options['collect_section'], links_body)[0])
+		else:
+			note = Note(contents=f"# {dest}\n{text}")
+			self.note_actions[note].append(lambda c: c)
+			
 
 	def append_prepend(self, match, notes):
 		do, to, tag, what = match.group(*'do to tag what'.split())
 		do = do.lower()
 		to = to.lower()
-		
-		print(f"got {len(notes)}")
 		
 		for note in notes.values():
 			if tag.strip('#') not in note.tags:
@@ -71,10 +89,13 @@ class NoteActions(NotesProcessor):
 				if do == 'append':
 					print(f"append to {note.title}")
 					self.note_actions[note].append(lambda c:
-						re.subn('\n', what+'\n', c, count=1))
+						re.subn('\n', what+'\n', c, count=1)[0])
 				elif do == 'prepend':
 					self.note_actions[note].append(lambda c:
 						re.sub('^', what, c))
+				elif do == 'remove' and re.match(rf'^(?:{what}|[^\n]+{what}\n)'):
+					self.note_actions[note].append(lambda c:
+						re.sub(rf'^(?:{what}|([^\n]+){what}(?=\n))', r'\1', c))
 						
 						
 	def search(self, match, notes):
@@ -82,12 +103,12 @@ class NoteActions(NotesProcessor):
 		for note in notes.values():
 			if note.title == self.options['action_note']:
 				 continue
-			if (where == 'everywhere' or where.startswith('#') and where.strip('#') in note.tags or where.startswith('[[') and match.group('link') == note.title) and term in note.contents:
+			if (where == 'everywhere' or where.startswith('#') and where.strip('#') in note.tags or where.startswith('[[') and where == f"[[{note.title}]]") and term in note.contents:
 				if strike:
 					#Remove highlights
 					self.note_actions[note].append(lambda c:
-						re.sub(rf"\b::{term}::\b", term, c))
+						re.sub(rf"::{term}::", term, c))
 				else:
 					#Use a lambda in sub to avoid highlighting already-highlighted terms or inside links
 					self.note_actions[note].append(lambda c:
-						re.sub(rf"::{term}::|\[\[[^\]]+]]|\b({term})\b", lambda m:"::\1::" if m.group(1) else m.group(0), c))
+						re.sub(rf"::.+?::|\[\[[^\]]+]]|\b({term})\b", lambda m:f"::{m.group(1)}::" if m.group(1) else m.group(0), c))
